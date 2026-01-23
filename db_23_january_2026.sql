4506c1b4efee8c63bfba1585310c79def16afb36
Denysov Maksym <93475429+Javac-g@users.noreply.github.com>
2026-01-23 20:15:45 +0200

    Clean up blank lines in db_23_january_2026.sql
    Removed unnecessary blank lines at the beginning of the file.

---
+++ db_23_january_2026.sql
@@ -0,0 +1,492 @@
+CREATE TYPE payment_status AS ENUM ('pending','paid','failed','canceled','refunded');
+CREATE TYPE subscription_status AS ENUM ('active','expired','canceled','pending');
+CREATE TYPE service_type_enum AS ENUM ('domain', 'ssl', 'hosting', 'email');
+CREATE TYPE service_status AS ENUM ('active','pending','expired','canceled','suspended');
+CREATE TYPE order_type_enum AS ENUM ('purchase', 'renewal', 'upgrade');
+CREATE TYPE user_status AS ENUM ('active','inactive','banned','pending','suspended');
+CREATE TYPE user_tier AS ENUM ('basic','premium','business','enterprise');
+CREATE TYPE domain_status AS ENUM ('active','expired','pending','canceled','suspended','grace');
+
+CREATE TABLE roles(
+	id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+	name varchar(25) UNIQUE NOT NULL,
+	description varchar(255)
+
+);
+CREATE TABLE credentials(
+	id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+	role_id BIGINT NOT NULL REFERENCES roles(id),
+	login varchar(60) UNIQUE NOT NULL,
+	email varchar(255) UNIQUE NOT NULL,
+	password_hash varchar(255) NOT NULL
+	
+);
+CREATE TABLE users(
+	id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+	credential_id BIGINT UNIQUE NOT NULL REFERENCES credentials(id) ON DELETE CASCADE,
+	full_name varchar(255),
+	created_at TIMESTAMPTZ NOT NULL DEFAULT now(),
+	updated_at TIMESTAMPTZ NOT NULL DEFAULT now(),
+	status user_status NOT NULL DEFAULT 'active',
+	tier user_tier NOT NULL DEFAULT 'basic'
+	
+);
+
+CREATE TABLE vendors(
+	id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+	login varchar(255) UNIQUE DEFAULT 'none',
+	email varchar(255) UNIQUE NOT NULL,
+	password varchar(255) NOT NULL
+	
+);
+CREATE TABLE vendors_credentials(
+	id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+	pines_cred BIGINT UNIQUE NOT NULL REFERENCES credentials(id) ON DELETE CASCADE,
+	vendors_cred BIGINT UNIQUE NOT NULL REFERENCES vendors(id) ON DELETE CASCADE
+
+);
+
+
+
+
+CREATE TABLE domains (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+	user_id BIGINT  NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+    name VARCHAR(255) NOT NULL,                     -- "example"
+    tld VARCHAR(50) NOT NULL,                       -- ".com"
+    fqdn VARCHAR(310) GENERATED ALWAYS AS (name || tld) STORED,
+   
+    expires_at TIMESTAMP NOT NULL,
+    renewal_date TIMESTAMP NOT NULL,
+    price NUMERIC(10,2) NOT NULL,
+    renewal_fee NUMERIC(10,2) NOT NULL,
+    redemption_fee NUMERIC(10,2) NOT NULL,
+    auto_renew BOOLEAN DEFAULT TRUE,
+    transfer_lock BOOLEAN DEFAULT TRUE,
+	transfer_lock_days INT DEFAULT 60,
+    status domain_status NOT NULL DEFAULT 'active',                    -- active, expired, grace, etc.
+    nameservers TEXT[] DEFAULT ARRAY[]::TEXT[],
+    contacts JSONB NOT NULL,                        -- registrant/admin/tech/billing contacts
+    epp_code VARCHAR(255),
+    registrar VARCHAR(100) NOT NULL,
+    is_legal_flagged BOOLEAN DEFAULT FALSE,         -- flagged for legal or abuse issues
+    legal_notes TEXT,                               -- description, case ID, DMCA details, etc.
+    created TIMESTAMP DEFAULT NOW(),
+    updated TIMESTAMP DEFAULT NOW()
+);
+
+
+
+
+CREATE TABLE private_email_services (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    user_id BIGINT NOT NULL REFERENCES users(id) ON UPDATE CASCADE ON DELETE CASCADE,
+    domain_id BIGINT REFERENCES domains(id) ON UPDATE CASCADE ON DELETE SET NULL,  -- optional link
+    service_name VARCHAR(255) NOT NULL,  -- e.g., "Private Email Pro", "Mailbox Plan"
+    
+    mailbox_count INT NOT NULL DEFAULT 1,  -- number of mailboxes included in this service
+    
+    created_at TIMESTAMPTZ NOT NULL DEFAULT now(),
+    updated_at TIMESTAMPTZ NOT NULL DEFAULT now(),
+    renewal_at TIMESTAMPTZ,
+    expires_at TIMESTAMPTZ,
+    
+    status service_status NOT NULL DEFAULT 'active',
+    warnings TEXT[] NOT NULL DEFAULT '{}',
+    
+    CHECK (expires_at IS NULL OR expires_at > created_at)
+);
+
+
+CREATE TABLE hosting_services (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    user_id BIGINT NOT NULL REFERENCES users(id) ON UPDATE CASCADE ON DELETE CASCADE,
+    domain_id BIGINT REFERENCES domains(id) ON UPDATE CASCADE ON DELETE SET NULL,  -- optional link
+    service_name VARCHAR(255) NOT NULL,   -- e.g., "Stellar Shared", "VPS-2"
+    
+    website_count INT NOT NULL DEFAULT 1,  -- number of websites allowed
+    storage_gb NUMERIC(10,2) NOT NULL DEFAULT 5,  -- storage in GB, default 5GB
+    
+    created_at TIMESTAMPTZ NOT NULL DEFAULT now(),
+    updated_at TIMESTAMPTZ NOT NULL DEFAULT now(),
+    renewal_at TIMESTAMPTZ,
+    expires_at TIMESTAMPTZ,
+    
+    status service_status NOT NULL DEFAULT 'active',
+    warnings TEXT[] NOT NULL DEFAULT '{}',
+    
+    CHECK (expires_at IS NULL OR expires_at > created_at)
+);
+
+CREATE TABLE ssl_certificates (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    user_id BIGINT NOT NULL REFERENCES users(id) ON UPDATE CASCADE ON DELETE CASCADE,
+    domain_id BIGINT REFERENCES domains(id) ON UPDATE CASCADE ON DELETE SET NULL, -- optional link
+    
+    certificate_cn VARCHAR(255) NOT NULL,   -- Common Name (CN)
+    certificate_type VARCHAR(20) NOT NULL DEFAULT 'DV',  -- DV/OV/EV
+    validity_days INT NOT NULL DEFAULT 365,
+    wildcard BOOLEAN NOT NULL DEFAULT FALSE,
+    
+    created_at TIMESTAMPTZ NOT NULL DEFAULT now(),  -- issued/created time
+    updated_at TIMESTAMPTZ NOT NULL DEFAULT now(),
+    renewal_at TIMESTAMPTZ,
+    expires_at TIMESTAMPTZ,
+    
+    status service_status NOT NULL DEFAULT 'active',
+    warnings TEXT[] NOT NULL DEFAULT '{}',
+    
+    CHECK (expires_at IS NULL OR expires_at > created_at)
+);
+
+CREATE TABLE apps_services (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    user_id BIGINT NOT NULL REFERENCES users(id) ON UPDATE CASCADE ON DELETE CASCADE,
+    service_name VARCHAR(255) NOT NULL,          -- e.g., "VPN Pro", "Cloud Backup"
+    created_at TIMESTAMPTZ NOT NULL DEFAULT now(),
+    updated_at TIMESTAMPTZ NOT NULL DEFAULT now(),
+    renewal_at TIMESTAMPTZ,                     -- optional renewal date
+    expires_at TIMESTAMPTZ,                     -- optional expiration
+    status service_status NOT NULL DEFAULT 'active',
+    warnings TEXT[] NOT NULL DEFAULT '{}',       -- any warnings or notes
+    CHECK (expires_at IS NULL OR expires_at > created_at)
+);
+CREATE TABLE membership_plans (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    plan_name VARCHAR(25) NOT NULL UNIQUE,
+    description TEXT,
+    duration INTERVAL NOT NULL,
+    cost NUMERIC(12,2) NOT NULL,
+	service_fee NUMERIC(12,2) NOT NULL DEFAULT 0,
+    currency CHAR(3) NOT NULL DEFAULT 'USD',
+    created_at TIMESTAMPTZ NOT NULL DEFAULT now(),
+    updated_at TIMESTAMPTZ NOT NULL DEFAULT now()
+);
+CREATE TABLE subscriptions (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    user_id BIGINT NOT NULL REFERENCES users(id) ON UPDATE CASCADE ON DELETE CASCADE,
+    plan_id BIGINT NOT NULL REFERENCES membership_plans(id) ON UPDATE CASCADE,
+    start_at TIMESTAMPTZ NOT NULL DEFAULT now(),
+    end_at TIMESTAMPTZ NOT NULL,             -- enforce end > start
+    status subscription_status NOT NULL DEFAULT 'active',
+    CHECK (end_at > start_at)
+);
+
+
+
+
+CREATE TABLE payments (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    user_id BIGINT NOT NULL REFERENCES users(id) ON UPDATE CASCADE ON DELETE CASCADE,
+    subscription_id BIGINT REFERENCES subscriptions(id) ON UPDATE CASCADE ON DELETE SET NULL,
+    payment_type VARCHAR(25) NOT NULL,        -- could be ENUM or table
+    amount NUMERIC(12,2) NOT NULL CHECK (amount > 0),
+    currency CHAR(3) NOT NULL DEFAULT 'USD',
+    status payment_status NOT NULL DEFAULT 'pending',
+    external_id VARCHAR(100),                 -- gateway transaction id
+    made_at TIMESTAMPTZ NOT NULL DEFAULT now(),
+    description VARCHAR(255)
+);
+
+
+CREATE TABLE orders (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    user_id BIGINT NOT NULL REFERENCES users(id) ON UPDATE CASCADE ON DELETE CASCADE,
+    service_type service_type_enum NOT NULL,
+    service_id BIGINT NOT NULL,
+    payment_id BIGINT REFERENCES payments(id) ON UPDATE CASCADE ON DELETE SET NULL,
+    order_type order_type_enum NOT NULL DEFAULT 'purchase',
+    subscription_fee NUMERIC(12,2),           -- fee based on subscription
+    discount_applied NUMERIC(12,2) DEFAULT 0,
+    total_amount NUMERIC(12,2) NOT NULL,
+    currency CHAR(3) NOT NULL DEFAULT 'USD',
+    created_at TIMESTAMPTZ NOT NULL DEFAULT now(),
+    renewed_at TIMESTAMPTZ,
+    expires_at TIMESTAMPTZ,
+    status payment_status NOT NULL DEFAULT 'pending'
+);
+
+
+CREATE OR REPLACE FUNCTION set_subscription_end_at()
+RETURNS trigger AS $$
+BEGIN
+    IF NEW.end_at IS NULL THEN
+        SELECT NEW.start_at + mp.duration INTO NEW.end_at
+        FROM membership_plans mp WHERE mp.id = NEW.plan_id;
+    END IF;
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER trg_set_subscription_end_at
+BEFORE INSERT ON subscriptions
+FOR EACH ROW
+EXECUTE FUNCTION set_subscription_end_at();
+
+CREATE OR REPLACE FUNCTION update_email_updated_at()
+RETURNS trigger AS $$
+BEGIN
+   NEW.updated_at := now();
+   RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER trg_update_updated_at
+BEFORE UPDATE ON private_email_services
+FOR EACH ROW EXECUTE FUNCTION update_email_updated_at();
+
+CREATE OR REPLACE FUNCTION validate_order_service()
+RETURNS trigger AS $$
+BEGIN
+  CASE NEW.service_type
+    WHEN 'domain' THEN
+      IF NOT EXISTS (SELECT 1 FROM domains WHERE id = NEW.service_id) THEN
+        RAISE EXCEPTION 'Invalid domain_id % for order %', NEW.service_id, NEW.id;
+      END IF;
+    WHEN 'ssl' THEN
+      IF NOT EXISTS (SELECT 1 FROM ssl_certificates WHERE id = NEW.service_id) THEN
+        RAISE EXCEPTION 'Invalid ssl_id % for order %', NEW.service_id, NEW.id;
+      END IF;
+    WHEN 'hosting' THEN
+      IF NOT EXISTS (SELECT 1 FROM hosting_services WHERE id = NEW.service_id) THEN
+        RAISE EXCEPTION 'Invalid hosting_id % for order %', NEW.service_id, NEW.id;
+      END IF;
+    WHEN 'email' THEN
+      IF NOT EXISTS (SELECT 1 FROM private_email_services WHERE id = NEW.service_id) THEN
+        RAISE EXCEPTION 'Invalid email_id % for order %', NEW.service_id, NEW.id;
+      END IF;
+  END CASE;
+  RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER trg_validate_order_service
+BEFORE INSERT OR UPDATE ON orders
+FOR EACH ROW EXECUTE FUNCTION validate_order_service();
+
+
+CREATE OR REPLACE FUNCTION apply_subscription_fee()
+RETURNS trigger AS $$
+DECLARE
+    per_order_fee NUMERIC(12,2);
+BEGIN
+    SELECT mp.service_fee INTO per_order_fee
+    FROM subscriptions s
+    JOIN membership_plans mp ON s.plan_id = mp.id
+    WHERE s.user_id = NEW.user_id
+      AND s.status = 'active'
+    LIMIT 1;
+
+    IF per_order_fee IS NULL THEN
+        per_order_fee := 0;
+    END IF;
+
+    NEW.subscription_fee := per_order_fee;
+    NEW.total_amount := NEW.total_amount + per_order_fee;
+
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER trg_apply_subscription_fee
+BEFORE INSERT ON orders
+FOR EACH ROW EXECUTE FUNCTION apply_subscription_fee();
+
+CREATE INDEX idx_credentials_login ON credentials(login);
+CREATE INDEX idx_credentials_email ON credentials(email);
+CREATE INDEX idx_users_credential_id ON users(credential_id);
+CREATE INDEX idx_user_roles_role_id ON roles(id);
+CREATE INDEX idx_pemail_user_status   ON private_email_services(user_id, status);
+CREATE INDEX idx_pemail_expires       ON private_email_services(expires_at);
+CREATE INDEX idx_pemail_domain_status ON private_email_services(domain_id, status);
+CREATE INDEX idx_hosting_user_status   ON hosting_services(user_id, status);
+CREATE INDEX idx_hosting_expires       ON hosting_services(expires_at);
+CREATE INDEX idx_hosting_domain_status ON hosting_services(domain_id, status);
+CREATE INDEX idx_ssl_user_status     ON ssl_certificates(user_id, status);
+CREATE INDEX idx_ssl_domain_status   ON ssl_certificates(domain_id, status);
+CREATE INDEX idx_ssl_expires         ON ssl_certificates(expires_at);
+CREATE INDEX idx_domains_user_status ON domains(user_id, status);
+CREATE INDEX idx_domains_expires     ON domains(expires_at);
+CREATE INDEX idx_subscriptions_user_status ON subscriptions(user_id, status);
+CREATE INDEX idx_subscriptions_plan_status ON subscriptions(plan_id, status);
+CREATE INDEX idx_payments_user_status          ON payments(user_id, status);
+CREATE INDEX idx_payments_subscription_status  ON payments(subscription_id, status);
+CREATE INDEX idx_orders_user_status          ON orders(user_id, status);
+CREATE INDEX idx_orders_service_type_status ON orders(service_type, status);
+CREATE INDEX idx_orders_payment_id          ON orders(payment_id);
+CREATE INDEX idx_orders_expires             ON orders(expires_at);
+
+INSERT INTO roles(name,description) VALUES ('USER','customer role');
+SELECT * FROM membership_plans ;
+
+CREATE TABLE purchase_fees (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    plan_id BIGINT NOT NULL
+        REFERENCES membership_plans(id) ON DELETE CASCADE,
+
+    min_amount NUMERIC(12,2) NOT NULL,
+    max_amount NUMERIC(12,2),         -- NULL = no upper limit
+    fee_amount NUMERIC(12,2) NOT NULL,
+
+    currency CHAR(3) NOT NULL DEFAULT 'USD',
+
+    CHECK (min_amount >= 0),
+    CHECK (max_amount IS NULL OR max_amount > min_amount)
+);
+
+INSERT INTO purchase_fees (
+    plan_id, min_amount, max_amount, fee_amount
+)
+VALUES
+    (1, 0.00, 100.00, 5.00),
+    (1, 100.00, NULL, 15.00);
+
+	ALTER TABLE membership_plans
+DROP COLUMN service_fee;
+purchase_fees
+
+INSERT INTO membership_plans (
+    plan_name,
+    description,
+    duration,
+    cost,
+    currency
+)
+VALUES (
+    'BASIC',
+    'For individuals who want to try out Pines Digital.
+Up to 1 person (1 Owner / 1 Admin).
+
+Includes:
+- Domain name registration (standard TLDs)
+- WHOIS Privacy included by default
+- Email-based support (response within 48 hours)
+- Full technical access to services (DNS, email, hosting)
+- Client self-management via control panel
+- Writing version and difference management
+- Email notifications for contacts and deadlines
+- Order receipts management panel
+
+Price: $0/month (tax included).',
+    INTERVAL '1 month',
+    0.00,
+    'USD'
+);
+INSERT INTO membership_plans (
+    plan_name,
+    description,
+    duration,
+    cost,
+    currency
+)
+VALUES (
+    'PREMIUM',
+    'For individuals who manage a lot of projects.
+Up to 1 person (1 Owner / 2 Admins - Free)
+
+Includes all Basic features, plus:
+- Hosting configuration assistance
+- SSL certificate setup support
+- Private Email (PE) mailbox setup
+- Premium DNS included
+- DNS routing and SSL configuration support
+- Customer verification support & assistance
+- Access to premium domains
+- Email support (response within 24 hours)
+- No fee for purchases under $100
+- 10% fee for purchases over $100
+
+Price: $50/month (tax included).',
+    INTERVAL '1 month',
+    50.00,
+    'USD'
+);
+INSERT INTO purchase_fees (
+    plan_id, min_amount, max_amount, fee_amount, currency
+)
+VALUES
+    (2, 0.00, 100.00, 0.00, 'USD'),        -- transactions under $100
+    (2, 100.00, NULL, 10.00, 'USD');       -- transactions $100 and above (10% of amount handled in app)
+
+
+INSERT INTO membership_plans (
+    plan_name,
+    description,
+    duration,
+    cost,
+    currency
+)
+VALUES (
+    'BUSINESS',
+    'For corporate and business teams that manage domain names and IT services as a team.
+Up to 99 people, pay per member.
+
+Includes all Premium features, plus:
+- Free general TLD domain (.com/.net/.org) for 1st year
+- Premium domain search & handling
+- Business account setup & verification
+- Registrar flexibility (Namecheap, GoDaddy, etc.)
+- KYC & identity verification support
+- Support via email (2h response)
+- Only 10% fee for premium domain purchases
+- Priority support
+
+Price: $250 per person per month.',
+    INTERVAL '1 month',
+    250.00,
+    'USD'
+);
+INSERT INTO purchase_fees (
+    plan_id, min_amount, max_amount, fee_amount, currency
+)
+VALUES
+    (3, 0.00, NULL, 10.00, 'USD');  -- 10% fee for premium domain purchases
+
+	INSERT INTO membership_plans (
+    plan_name,
+    description,
+    duration,
+    cost,
+    currency
+)
+VALUES (
+    'ENTERPRISE',
+    'Tailored for large businesses and mission-critical platforms: managed infrastructure, compliance support, custom integrations, and dedicated engineering.
+
+Unlimited users. Annual or invoice payment. Bank transfer supported.
+
+Includes all Business features, plus:
+- Dedicated domain manager: renewals, DNS, support
+- Full domain lifecycle: acquire → renew → resell
+- Sanctioned region help: crypto, legal routing
+- Full hands-off service management
+- VIP support: 1h response SLA
+- Only 5% fee for premium domain purchases
+
+Price: $1000/month.',
+    INTERVAL '1 month',
+    1000.00,
+    'USD'
+);
+INSERT INTO purchase_fees (
+    plan_id, min_amount, max_amount, fee_amount, currency
+)
+VALUES
+    (4, 0.00, NULL, 5.00, 'USD'); 
+
+	SELECT * FROM membership_plans;
+UPDATE membership_plans
+SET duration = 
+    CASE plan_name
+        WHEN 'Basic' THEN 30
+        WHEN 'Premium' THEN 30
+        WHEN 'Business' THEN 30
+        WHEN 'Enterprise' THEN 30
+        ELSE 30
+    END;
+SELECT * FROM membership_plans;
+ALTER TABLE membership_plans
+ALTER COLUMN duration TYPE INTEGER USING EXTRACT(DAY FROM duration);
+
+
+

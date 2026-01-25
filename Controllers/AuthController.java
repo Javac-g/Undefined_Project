5d9c02b67ca8956a4c0fa4992cfc7589d817b59a
Denysov Maksym <93475429+Javac-g@users.noreply.github.com>
2026-01-26 01:20:05 +0200

    Implement AuthController for user registration and login

---
+++ Controllers/AuthController.java
@@ -0,0 +1,83 @@
+import com.denysov.essex.model.MembershipPlan;
+import com.denysov.essex.model.Subscription;
+import com.denysov.essex.model.SubscriptionStatus;
+import com.denysov.essex.model.User;
+import com.denysov.essex.repository.MembershipPlanRepository;
+import com.denysov.essex.repository.SubscriptionRepository;
+import com.denysov.essex.services.MembershipService;
+import com.denysov.essex.services.RegistrationService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+
+@Controller
+public class AuthController {
+
+    private final RegistrationService registrationService;
+    private final MembershipService membershipService;
+    private final SubscriptionRepository subscriptionRepository;
+
+    public AuthController(RegistrationService registrationService,
+                          MembershipService membershipService,
+                          SubscriptionRepository subscriptionRepository) {
+
+        this.membershipService = membershipService;
+        this.registrationService = registrationService;
+        this.subscriptionRepository = subscriptionRepository;
+    }
+
+    @GetMapping("/login")
+    public String loginPage() {
+        return "login";
+    }
+    @GetMapping("/main")
+    public String mainPage() {
+        return "main";
+    }
+
+
+    @GetMapping("/registration")
+    public String registerPage( Model model) {
+        model.addAttribute("plans", membershipService.getAll());
+        return "registration";
+    }
+
+    @PostMapping("/registration")
+    public String register(
+            @RequestParam String username,
+            @RequestParam String email,
+            @RequestParam String password,
+            @RequestParam String firstName,
+            @RequestParam String lastName,
+            @RequestParam Long planId,
+            Model model
+    ) {
+
+        try {
+           User user = registrationService.register(username,
+                    email,
+                    password,
+                    firstName + " " + lastName);
+            MembershipPlan plan = membershipService.getPlanById(planId);
+
+            Subscription sub = new Subscription();
+            sub.setUser(user);
+            sub.setPlan(plan);
+            Instant start = Instant.now();
+            sub.setStartAt(start);
+            sub.setEndAt(start.plus(plan.getDurationDays(), ChronoUnit.DAYS));
+            sub.setStatus(SubscriptionStatus.ACTIVE);
+
+            subscriptionRepository.save(sub);
+            return "redirect:/login?registered";
+        } catch (Exception ex) {
+            model.addAttribute("error", ex.getMessage());
+            return "registration";
+        }
+    }
+}

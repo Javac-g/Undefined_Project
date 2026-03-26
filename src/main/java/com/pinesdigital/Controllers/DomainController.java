92408107b0bd670fe87b0f3ac59839168efd79b0
Maksym Denysov <93475429+Javac-g@users.noreply.github.com>
2026-03-26 21:40:56 +0200

    Update DomainController.java

--- src/main/java/com/pinesdigital/Controllers/DomainController.java
+++ src/main/java/com/pinesdigital/Controllers/DomainController.java
@@ -2,6 +2,13 @@
 @RequestMapping("/dashboard/domains")  
 public class DomainController{
 
-
+    @GetMapping("/domains")
+    @ResponseBody
+    public SliceResponse<DomainSummaryDTO> loadDomains( @RequestParam(defaultValue = "0") int page) {
+      
+          Long userId = currentUser.getUserId();
+          return domainService.getSliceUserDomains(userId, page, 10);
+      
+        }
   
 }
